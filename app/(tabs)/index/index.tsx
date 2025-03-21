24ab7e5e6692489a76ab56c2aee2e435dd17db5d
thomasjeu <jeuthomas03@gmail.com>
2025-03-21 18:07:26 +0100

    WIP

--- app/(tabs)/index/index.tsx
+++ app/(tabs)/index/index.tsx
@@ -7,6 +7,7 @@ import {Image} from "expo-image";
 export default function HomeScreen() {
     const { t } = useTranslations();
 
+    // TODO: animations + generate story form
     return (
         <>
             <SafeAreaView
